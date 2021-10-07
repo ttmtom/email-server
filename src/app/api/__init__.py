e1cb002b1454a333a86d93600d0ff50b19d7c2d1
Tom Tam <tmtam921@gamil.com>
2021-10-07 21:56:22 +0800

    add dto

--- src/app/api/__init__.py
+++ src/app/api/__init__.py
@@ -1,8 +1,16 @@
 from flask_restx import Api
-from app.api.server.controller import app as ns_server
-from app.api.mail.controller import app as ns_email
+from app.api.server.controller import setup_system_ns
+from app.api.mail.controller import setup_mail_ns
 from app.constants.path import MAIL_SERVICES_PATH, SERVER_SERVICES_PATH
 
-api = Api()
-api.add_namespace(ns_server, path=SERVER_SERVICES_PATH)
-api.add_namespace(ns_email, path=MAIL_SERVICES_PATH)
+
+def api_setup(flask_mail):
+    api = Api()
+
+    ns_server = setup_system_ns()
+    api.add_namespace(ns_server, path=SERVER_SERVICES_PATH)
+
+    ns_email = setup_mail_ns(flask_mail)
+    api.add_namespace(ns_email, path=MAIL_SERVICES_PATH)
+
+    return api
--- src/app/api/mail/controller.py
+++ src/app/api/mail/controller.py
@@ -3,41 +3,26 @@ from app.api.mail.service import sendEmail
 from app.constants.path import MAIL_SERVICES_PATH
 from app.api.mail.model import Model
 
-app = Namespace(MAIL_SERVICES_PATH, description='mail services')
-apiModel = Model(app)
+def setup_mail_ns (flask_mail):
+    app = Namespace(MAIL_SERVICES_PATH, description='mail services')
+    apiModel = Model(app)
 
-@app.route('/send', methods=['POST'])
-class Server(Resource):
-    @app.response(200, 'Success', apiModel.sendSuccessModel)
-    @app.response(400, 'Bad args', apiModel.badArgsMode)
-    @app.doc(params={'target': 'target email address'})
-    def post(self):
-        print('post send email')
-        parser = reqparse.RequestParser()
-        parser.add_argument('target', type=str, required=True,  help='target email address')
-        args = parser.parse_args()
+    @app.route('/send', methods=['POST'])
+    class Server(Resource):
+        @app.response(200, 'Success', apiModel.sendSuccessModel)
+        @app.response(400, 'Bad args', apiModel.badArgsMode)
+        @app.doc(params={'target': 'target email address'})
+        def post(self):
+            print('post send email')
+            parser = reqparse.RequestParser()
+            parser.add_argument('target', type=str, required=True,  help='target email address')
+            args = parser.parse_args()
 
-        print(args)
-        sendEmail(args.target, 'hii')
+            print(args)
+            sendEmail(args.target, 'hii')
 
-        return {
-            'success': True
-        }
+            return {
+                'success': True
+            }
 
-@app.route('/test')
-class Server(Resource):
-    @app.response(200, 'Success', apiModel.sendSuccessModel)
-    @app.response(400, 'Bad args', apiModel.badArgsMode)
-    @app.doc(params={'target': 'target email address'})
-    def post(self):
-        print('post send email')
-        parser = reqparse.RequestParser()
-        parser.add_argument('target', type=str, required=True,  help='target email address')
-        args = parser.parse_args()
-
-        print(args)
-        sendEmail(args.target, 'hii')
-
-        return {
-            'success': True
-        }
+    return app
